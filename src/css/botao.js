5ec841bfda5d85bf02f5b37ada9f6e570b761996
Dominic Silva <166265205+mahfsilva@users.noreply.github.com>
2024-04-08 16:56:03 -0300

    Add files via upload

---
+++ src/css/botao.js
@@ -0,0 +1,20 @@
+    var botao1 = document.getElementById("botaoGit");
+    var botao2 = document.getElementById("botaoMentor");
+    var botao3 = document.getElementById("botaoIn");
+    var botao4 = document.getElementById("botaoInsta");
+
+    botao1.addEventListener("click", function() {
+        window.open("https://github.com/mahfsilva", "_blank");
+    });
+
+    botao2.addEventListener("click", function() {
+        window.open("https://www.frontendmentor.io/profile/mahfsilva", "_blank");
+    });
+
+    botao3.addEventListener("click", function() {
+        window.open("https://www.linkedin.com/in/mariana-silva-1b6124302/", "_blank");
+    });
+
+    botao4.addEventListener("click", function() {
+        window.open("https://www.instagram.com/mah_fsilva/", "_blank");
+    }); 
